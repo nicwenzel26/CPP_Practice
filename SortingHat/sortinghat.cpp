d45636476243c4717720d33cf23748f194174fa4
nicwenzel26 <nwenzel@mymail.mines.edu>
2020-10-07 16:17:09 -0600

    Finished Sorting hat, works remote, does not work local

--- SortingHat/sortinghat.cpp
+++ SortingHat/sortinghat.cpp
@@ -24,20 +24,20 @@ int main() {
 
   switch (answer1) {
   case 1:
-  hufflepuff += 1;
-  break;
+    hufflepuff += 1;
+    break;
   case 2:
-  slytherin += 1;
-  break;
+    slytherin += 1;
+    break;
   case 3:
-  ravenclaw += 1;
-  break;
+    ravenclaw += 1;
+    break;
   case 4:
-  gryffindor += 1;
-  break;
+    gryffindor += 1;
+    break;
   default:
-  cout << "Invalid answer.\n";
-  return 1;
+    cout << "Invalid answer.\n";
+    return 1;
 }
 
   //Question 2
@@ -61,6 +61,96 @@ int main() {
     cout << "Invalid Input\n";
     return 1;
   }
+
+  //Question 3
+  cout << "Q3) Which kind of instrument most pleases your ear?\n";
+  cout << "1) The violin\n";
+  cout << "2) The trumpet\n";
+  cout << "3) The piano\n";
+  cout << "4) The drum\n";
+  cout << "Answer? ";
+  cin >> answer3;
+
+  switch (answer3) {
+    case 1:
+      slytherin += 1;
+      break;
+    case 2:
+      hufflepuff += 1;
+      break;
+    case 3:
+      ravenclaw += 1;
+      break;
+    case 4:
+      gryffindor += 1;
+      break;
+    default:
+      cout << "Invalid input\n";
+      return 1;
+  }
+
+  //Question 4
+  cout << "Q4) Which road tempts you the most?\n";
+  cout << "1) The wide sunny grassy lane\n";
+  cout << "2) The narrow, dark, lantern lit alley\n";
+  cout << "3) The twisting, leaf strewn path through the woods\n";
+  cout << "4) The coubbled steet\n";
+  cout << "Answer? ";
+  cin >> answer4;
+
+  switch (answer4) {
+    case 1:
+      hufflepuff += 1;
+      break;
+    case 2:
+      slytherin += 1;
+      break;
+    case 3:
+      gryffindor += 1;
+      break;
+    case 4:
+      ravenclaw += 1;
+      break;
+    default:
+      cout << "Invalid input\n";
+      return 1;
+  }
+
+
+  //Determine house
+  string house;
+  int max = 0;
+
+  if (gryffindor > max) {
+
+    max = gryffindor;
+    house = "Gryffindor";
+
+  }
+
+  if (hufflepuff > max) {
+
+    max = hufflepuff;
+    house = "Hufflepuff";
+
+  }
+
+  if (ravenclaw > max) {
+
+    max = ravenclaw;
+    house = "Ravenclaw";
+
+  }
+
+  if (slytherin > max) {
+
+    max = slytherin;
+    house = "Slytherin";
+
+  }
+
+
+  cout << "Your house is " << house << "!\n";
 
   return 0;
 }
